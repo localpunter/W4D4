c8440853cec158f6e38e8217fa44df4202b7e004
Alan Hunter <localpunter@sky.com>
2018-08-25 11:48:48 +0100

    many to many zombies

---
+++ many_to_many_sinatra_zombies/models/action.rb
@@ -0,0 +1,11 @@
+class Action
+  def self.all
+    return ["ate",
+            "chased",
+            "nibbled",
+            "drooled on",
+            "chewed on",
+            "ripped the heart out of",
+            "fried"]
+  end
+end
